92c2810315e7146e9f18672d86fac97c9d5dff0c
Matthew Berthoud <mwberthoud@wm.edu>
2024-09-03 11:06:52 -0400

    extracting bounds

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import xml.etree.ElementTree as ET
+import pprint
 import os
 
 def do_parse(data_dir, f):
     print("Parsing " + f)
     try:
-        ET.parse(data_dir + f) 
+        return ET.parse(data_dir + f) 
     except:
         print("Error while parsing, aborting")
 
 
 data_dir = "Programming-Assignment-Data/"
-trees = [do_parse(data_dir, f) for f in os.listdir(data_dir) if f.endswith(".xml")]
+filenames = [f[:-4] for f in os.listdir(data_dir) if f.endswith(".xml")]
+
+for f in filenames:
+    box_coords = []
+
+    tree = do_parse(data_dir, f + ".xml")
+    root = tree.getroot()
+    elements = root.iter()
+    for element in elements:
+        # print("\n" + str(element.tag))
+        # pprint.pp(element.attrib)
+        is_leaf = True
+        for child in element:
+            is_leaf = False
+            break
+        if is_leaf:
+            bounds = element.attrib.get("bounds")
+            bounds = bounds.replace('][', '],[')
+            bounds = eval(bounds)
+            print(bounds)
